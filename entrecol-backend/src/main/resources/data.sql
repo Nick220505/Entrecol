949ad38a4dbf1962022bc02f89cd6fd632d4fd29
Nick220505 <juannicolaspardo@gmail.com>
2024-10-20 03:08:56 -0500

    data.sql script added

---
+++ entrecol-backend/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" (username, password, email) VALUES
+('admin', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'admin@entrecol.com'),
+('user1', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'user1@entrecol.com'),
+('user2', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'user2@entrecol.com');
