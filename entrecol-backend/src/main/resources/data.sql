27d90e266934162429bdfff5f2d76c8467ee5113
Nick220505 <juannicolaspardo@gmail.com>
2024-10-20 03:19:52 -0500

    Inserts updated for user table

--- entrecol-backend/src/main/resources/data.sql
+++ entrecol-backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO "user" (username, password, email) VALUES
-('admin', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'admin@entrecol.com'),
-('user1', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'user1@entrecol.com'),
-('user2', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'user2@entrecol.com');
+('usuario1', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'usuario1@entrecol.com'),
+('usuario2', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'usuario2@entrecol.com'),
+('usuario3', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'usuario3@entrecol.com');
