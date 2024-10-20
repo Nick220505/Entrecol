a42d08b21aa99bf73f4b62d8ab866c063cea5b11
Nick220505 <juannicolaspardo@gmail.com>
2024-10-19 21:17:10 -0500

    User schema updated

--- entrecol-backend/src/main/java/co/edu/unbosque/model/User.java
+++ entrecol-backend/src/main/java/co/edu/unbosque/model/User.java
@@ -12,19 +12,19 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "user") // Comment
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, length = 50)
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String password;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, length = 100)
     private String email;
 
     @Column(name = "created_at")
