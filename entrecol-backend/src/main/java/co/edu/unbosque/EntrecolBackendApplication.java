c665d69faca7dfa92c818a891b4e6c6097981302
Nick220505 <juannicolaspardo@gmail.com>
2024-10-23 23:49:14 -0500

    Docker updated for backend

--- entrecol-backend/src/main/java/co/edu/unbosque/EntrecolBackendApplication.java
+++ entrecol-backend/src/main/java/co/edu/unbosque/EntrecolBackendApplication.java
@@ -2,10 +2,8 @@ package co.edu.unbosque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class EntrecolBackendApplication {
 
 	public static void main(String[] args) {
--- entrecol-backend/src/main/java/co/edu/unbosque/security/CustomUserDetailsService.java
+++ entrecol-backend/src/main/java/co/edu/unbosque/security/CustomUserDetailsService.java
@@ -24,6 +24,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
     }
 }
