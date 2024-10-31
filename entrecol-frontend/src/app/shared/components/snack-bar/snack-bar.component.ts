f0f2c9d5741e7200f0e075283e664941598b81ab
Nick220505 <juannicolaspardo@gmail.com>
2024-10-31 18:44:07 -0500

    Snack bar component updated

--- entrecol-frontend/src/app/shared/components/snack-bar/snack-bar.component.ts
+++ entrecol-frontend/src/app/shared/components/snack-bar/snack-bar.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {
@@ -20,6 +20,7 @@ interface SnackBarData {
   imports: [NgClass, MatButtonModule, MatIconModule],
   templateUrl: './snack-bar.component.html',
   styleUrl: './snack-bar.component.scss',
+  encapsulation: ViewEncapsulation.None,
 })
 export class SnackBarComponent {
   protected readonly snackBarRef = inject(MatSnackBarRef);
