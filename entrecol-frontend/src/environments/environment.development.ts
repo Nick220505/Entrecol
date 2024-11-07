77966f0ca54821d5411ca88cd9ba6005df1fe584
Nick220505 <juannicolaspardo@gmail.com>
2024-11-07 11:28:11 -0500

    Configurations updated to work with github codespaces

--- entrecol-frontend/src/environments/environment.development.ts
+++ entrecol-frontend/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: typeof window !== 'undefined' && window.location.hostname.includes('github.dev')
+    ? `https://${window.location.hostname.replace('-4200', '-8080')}/api`
+    : 'http://localhost:8080/api',
 };
