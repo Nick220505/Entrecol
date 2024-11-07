6e87355da151f772e456da13f78239eb2e7a6538
Nick220505 <juannicolaspardo@gmail.com>
2024-11-07 11:54:07 -0500

    Database configuration updated

--- docker/mysql/init/2-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` (username, password, email) VALUES
-('usuario1', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'usuario1@entrecol.com'),
-('usuario2', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'usuario2@entrecol.com'),
-('usuario3', '$2a$10$0hxNcNcGj9Jgd9AfyBRVwOPeQeeLCVn16UouBctICB.aWpxMJkDT2', 'usuario3@entrecol.com');
