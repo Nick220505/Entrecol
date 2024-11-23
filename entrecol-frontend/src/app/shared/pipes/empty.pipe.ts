b4cd86c21b32f6516a4c4b5114bb981142b3b290
Nick220505 <juannicolaspardo@gmail.com>
2024-11-23 14:57:36 -0500

    Pipe migrated to standalone

--- entrecol-frontend/src/app/shared/pipes/empty.pipe.ts
+++ entrecol-frontend/src/app/shared/pipes/empty.pipe.ts
@@ -1,9 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'empty',
-  standalone: true,
-})
+@Pipe({ name: 'empty' })
 export class EmptyPipe implements PipeTransform {
   transform(value: unknown): string {
     if (value === null || value === undefined || value === '') {
